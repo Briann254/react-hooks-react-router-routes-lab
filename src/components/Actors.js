0403b3da6447db9b6dadac91375d491662e4df74
git_bnderitu <bnnderitu01@email.com>
2023-01-16 22:38:25 +0300

    completed Router-routes lab

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
-}
-
+    return (
+      <div>
+        <h1>Actors Page</h1>
+        {actors.map((actor, index) => (
+            <div key={index}>
+                <h3>Name: {actor.name}</h3>
+                <p>Movies:</p>
+                <ul>
+                    {actor.movies.map((movie, index) => (
+                        <li key={index}>{movie}</li>
+                    ))}
+                </ul>
+            </div>
+        ))}
+        {/*{code here}*/}
+      </div>
+    );
+  };
+  
 export default Actors;
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import { 
+  BrowserRouter as Router, 
+  Route
+} from 'react-router-dom';
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Actors from "./Actors";
 import Directors from "./Directors";
 import Movies from "./Movies";
 
-function App() {
-  return <div>{/*{code here}*/}</div>;
-}
+function App(props) {
+  rreturn (
+    <Router>
+      <div className="app">
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/actors" component={Actors} />
+        <Route exact path="/directors" component={Directors} />
+        <Route exact path="/movies" component={Movies} />
+      </div>
+      {/*{code here}*/}
+    </Router>
+  );
+};
 
 export default App;
